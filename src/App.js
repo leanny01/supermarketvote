c411e497216ebed5bcd6462cd8b97752488ea1b0
leanny01 <dleannykalangu@gmail.com>
2020-12-06 13:34:59 +0200

    added semantic UI and logos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react';
 
 import superMarketList from './List'
+
 import './App.css';
 
 
@@ -30,9 +31,8 @@ const SuperMarketList = ()=>{
 
     setSuperMarkets(nextState)
 
-
-
 }
+
 
  
   return (
--- src/List.js
+++ src/List.js
@@ -1,43 +1,57 @@
 
-     const  generateVoteCount = ()=> {
+    import  checkersLogo from './images/superMarkets/checkers.jpg'
+    import cambridgeLogo from './images/superMarkets/cambridge.png'
+    import boxerLogo from './images/superMarkets/logo-boxer.png'
+    import pnpLogo from './images/superMarkets/pnp.png'
+    import  shopriteLogo from  './images/superMarkets/shoprite.png'
+    import sheckersHyperLogo from './images/superMarkets/supermarket-hyper.jpg'
+
+    import daniel from './images/avatars/daniel.jpg'
+    import kristy from './images/avatars/kristy.png'
+    import veronika from './images/avatars/veronika.jpg'
+    import molly from './images/avatars/molly.png'
+
+
+
+    const  generateVoteCount = ()=> {
       return Math.floor((Math.random() * 50) + 15);
     }
     const superMarketList = [
       {
         id: 1,
-        title: 'Yellow Pail',
-        description: 'On-demand sand castle construction expertise.',
+        title: 'Checkers',
+        description: 'Checkers is a FMCG retailer owned by Shoprite that operates in Southern Africa. Checkers currently has 37 Checkers Hypers and 202 Checkers supermarkets operating in Botswana, South Africa and Namibia.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'images/avatars/daniel.jpg',
-        superMarketImageUrl: 'images/superMarkets/image-aqua.png',
+        submitterAvatarUrl: daniel,
+        superMarketImageUrl: checkersLogo,
       },
       {
         id: 2,
-        title: 'Supermajority: The Fantasy Congress League',
-        description: 'Earn points when your favorite politicians pass legislation.',
+        title: 'Cambridge',
+        description: 'Earn points when you shop at your favorite Cambridge supermarket store.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'images/avatars/kristy.png',
-        superMarketImageUrl: 'images/superMarkets/image-rose.png',
+        submitterAvatarUrl: kristy,
+        superMarketImageUrl: cambridgeLogo,
       },
       {
         id: 3,
-        title: 'Tinfoild: Tailored tinfoil hats',
-        description: 'We already have your measurements and shipping address.',
+        title: 'Boxer',
+        description: 'We already have your shipping address.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'images/avatars/veronika.jpg',
-        superMarketImageUrl: 'images/superMarkets/image-steel.png',
+        submitterAvatarUrl: veronika,
+        superMarketImageUrl: boxerLogo,
       },
       {
         id: 4,
-        title: 'Haught or Naught',
+        title: 'Pick and Pay',
         description: 'High-minded or absent-minded? You decide.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'images/avatars/molly.png',
-        superMarketImageUrl: 'images/superMarkets/image-yellow.png',
+        submitterAvatarUrl: molly,
+        superMarketImageUrl: pnpLogo,
       },
     ];
   
